69e4dec6f0195cac1a083dd41fccfa40ca4e9522
Gevorg Melkumyan <gevorgmelkoumyan@gmail.com>
2024-08-26 18:55:58 +0400

    Installed horizon and websockets packages

--- routes/channels.php
+++ routes/channels.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+Route::middleware('cors')->group(function () {
+	Broadcast::routes();
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+	Broadcast::channel('lw.{user}', function (User $user) {
+		return true;
+	});
 });
